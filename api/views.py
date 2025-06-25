fdbad9de0a4ef74ad808e6c1af0538e4b9a7631b
Yashvant Raim <yashwantrai.mpcz@gmail.com>
2025-06-25 13:22:31 +0530

    new code add update and delete by class

--- api/views.py
+++ api/views.py
@@ -72,7 +72,22 @@ class EmployeeDetail(APIView):
     def get (self, request, pk):
         employee = self.get_object(pk)
         serializer = EmployeeSerializer(employee)
-        return Response(serializer.data, status=status.HTTP_200_OK)       
+        return Response(serializer.data, status=status.HTTP_200_OK)     
+
+    def put(self, request, pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)  
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)   
+    
+
+    def delete(self, request,pk):
+        employee = self.get_object(pk)
+        employee.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 
 
